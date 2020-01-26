e5364697f7faf2a8c11611eae14e9f1f2cf9bf11
unknown <johandata@yahoo.com>
2020-01-27 00:57:52 +0700

    first commit master

---
+++ application/models/Modelcentury.php
@@ -0,0 +1,101 @@
+<?php
+
+class Modelcentury extends CI_model{
+	
+
+		function showBellBoy(){
+
+			return $this->db->get("bellboy_master")->result();
+		}
+
+
+		function showKamar(){
+
+			return $this->db->get("master_kamar")->result();
+
+		}
+
+		function insertKamar($kodeKamar="", $typeKamar="" ){
+
+			$data = array(
+				"kode_kamar" => $kodeKamar,
+				"type_kamar" => $typeKamar
+			);
+
+			$this->db->insert("master_kamar",$data);
+
+		}
+
+
+
+
+		function insertKomentar($customer="", $komentar="" ){
+
+			$data = array(
+				"id_customer" => $customer,
+				"komentar" => $komentar
+			);
+
+			if($customer!="" && $komentar!=""){
+
+					$this->db->insert("penilaian",$data);
+
+			}
+	
+	}
+
+
+	
+	function modelLogin($email="" , $password=""){ 
+		$this->db->where("email_pegawai",$email);
+		$this->db->where("password_pegawai", $password);
+		return $this->db->get("master_pegawai")->num_rows();
+	}
+
+	function modelMasterPegawai(){
+
+		return $this->db->get("master_pegawai")->result();
+	}
+
+
+	function modelMasterJabatan(){
+
+		$this->db->where("status_jabatan",1);
+		
+		return $this->db->get("master_jabatan")->result();
+	}
+
+	function modelMasterKamar(){
+
+		return $this->db->get("master_kamar")->result();
+	}
+
+	function modelBellboyCommander(){
+
+		$this->db->select('*');
+		$this->db->from('trx_bellboy');
+		$this->db->join('master_pegawai', 'master_pegawai.nip_pegawai = trx_bellboy.id_commander', 'inner');
+		$this->db->join('trx_tamu', 'trx_tamu.kode_pesan = trx_bellboy.kode_pesan', 'inner');
+		
+		$query = $this->db->get();
+
+		return $query->result();
+
+	}
+
+	function modelBellboyAcceptor(){
+
+		$this->db->select('*');
+		$this->db->from('trx_bellboy');
+		$this->db->join('master_pegawai', 'master_pegawai.nip_pegawai = trx_bellboy.id_accepter', 'inner');
+		$this->db->join('trx_tamu', 'trx_tamu.kode_pesan = trx_bellboy.kode_pesan', 'inner');
+		
+		$query = $this->db->get();
+
+		return $query->result();
+
+	}
+
+
+
+}
